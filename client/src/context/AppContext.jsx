b3a0c9177e0125f96b37e8bf42e6cddf8c28e10e
Diganta Nayak <diganta.19032001@gmail.com>
2025-07-04 16:39:14 +0530

    Created The Navbar and managed login with Clerk

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react-refresh/only-export-components */
+import React from 'react'
+import { createContext } from "react";
+
+export const AppContext = createContext()
+
+export const  AppContextProvider = (props)=>{
+        const value = {
+
+        }
+
+        return(<AppContext.Provider value={value}>{props.children}</AppContext.Provider>)
+}
