c7faa293ea5f4cf3b3f3e1380b2709ba2b75c9e3
Diganta Nayak <diganta.19032001@gmail.com>
2025-08-11 00:08:40 +0530

    Company Login and Job posting done in the backend

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -2,6 +2,7 @@ import {v2 as cloudinary} from 'cloudinary';
 import Company from "../models/Company.js";
 import bcrypt from "bcrypt";
 import generateToken from '../utils/generateToken.js';
+import Job from '../models/Job.js';
 
 //Register a new Company
 export const registerCompany = async (req,res)=>{
@@ -48,7 +49,29 @@ export const registerCompany = async (req,res)=>{
 //Company Login
 
 export const loginCompany = async (req,res) =>{
+    const {email,password} = req.body
 
+    try {
+        const company = await Company.findOne({email});
+
+        if(bcrypt.compare(password, company.password)){
+            res.json({
+                success:true,
+                company:{
+                _id:company._id,
+                name:company.name,
+                email: company.email,
+                image:company.image
+                },
+                token: generateToken(company._id)
+            }
+        )
+        }else{
+            res.json({success:false,message:"Invalid email or password"})
+        }
+    } catch (error) {
+        res.json({success:false, message:error.message})
+    }
 }
 
 //Get Company Data
@@ -59,6 +82,29 @@ export const getCompanyData = async(req,res)=>{
 //Post a new Job
 export const postJob = async(req,res)=>{
 
+    const {title,description,location,salary,level,category} = req.body;
+
+    const companyId = req.company._id
+
+    // console.log(companyId,{title,description,location,salary});
+
+    try{
+        const newJob = new Job({
+            title,
+            description,
+            location,
+            salary,
+            companyId,
+            date:Date.now(),
+            level,
+            category
+        })
+        await newJob.save();
+        res.json({success:true,newJob})
+    }catch(error){
+        res.json({success:false,message:error.message})
+    }
+    
 }
 
 //Get Company Job Applicants
--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { ChangeJobApplicationStatus, changeVisibility, getCompantPostedJobs, getCompanyData, getCompanyJobApplicants, loginCompany, postJob, registerCompany } from "../controllers/companyController.js";
 import upload from "../config/multer.js";
+import { protectCompany } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
@@ -11,22 +12,22 @@ router.post('/register',upload.single("image"),registerCompany);
 router.post('/login',loginCompany);
 
 //Get Company Data
-router.get('/company',getCompanyData);
+router.get('/company',protectCompany,getCompanyData);
 
-router.post('/post-job',postJob);
+router.post('/post-job',protectCompany,postJob);
 
 //Get Applicants for a Job
-router.get('/applicants',getCompanyJobApplicants);
+router.get('/applicants',protectCompany,getCompanyJobApplicants);
 
 //Get Company posted Jobs
-router.get('/list-jobs',getCompantPostedJobs);
+router.get('/list-jobs',protectCompany,getCompantPostedJobs);
 
 
 //Change Application Status
-router.post('/change-status',ChangeJobApplicationStatus);
+router.post('/change-status',protectCompany,ChangeJobApplicationStatus);
 
 
 //Change Application Visibility
-router.post('change-visibility',changeVisibility);
+router.post('change-visibility',protectCompany,changeVisibility);
 
 export default router;
